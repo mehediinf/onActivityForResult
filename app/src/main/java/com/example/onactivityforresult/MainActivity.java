01039327a671e90691b11e3cd95847ca13c3aaa1
mehedi-hasan <mehediinform@gmail.com>
2024-01-11 05:31:55 +0600

    Second Commit onActivityForResult

--- app/src/main/java/com/example/onactivityforresult/MainActivity.java
+++ app/src/main/java/com/example/onactivityforresult/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.onactivityforresult;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private Button button;
+
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button1_Id);
+        textView = findViewById(R.id.textView1_Id);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                startActivityForResult(intent,1);
+
+            }
+        });
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode==1){
+            String value = data.getStringExtra("key");
+            textView.setText(value);
+        }
+
     }
 }
